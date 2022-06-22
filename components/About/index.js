150434b1dcac3a549aa303424bdf7b7b67f1ab07
Josh <joshuaalexanderneo@gmail.com>
2022-06-22 18:42:41 +0400

    add author picture

--- components/About/index.js
+++ components/About/index.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import KofiButton from 'kofi-button'
+import Image from 'next/image'
 
 const About = () => {
   return (
@@ -37,11 +38,13 @@ const About = () => {
 
           <div className='mt-8 lg:mt-0 lg:w-1/2'>
             <div className='flex items-center justify-center lg:justify-end'>
-              <div className='max-w-lg'>
-                <img
+              <div className='max-w-lg relative aspect-[3/4] h-96'>
+                <Image
                   className='object-cover object-center w-full h-64 rounded-md shadow'
-                  src='https://images.unsplash.com/photo-1484399172022-72a90b12e3c1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80'
+                  src={require('../../public/leif.jpg')}
                   alt=''
+                  layout='fill'
+                  objectFit='responsive'
                 />
               </div>
             </div>
