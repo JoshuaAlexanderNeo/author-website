b8819496fe3dfb276b44972cdc812dab7b0d35d5
Josh <joshuaalexanderneo@gmail.com>
2022-06-23 01:20:17 +0400

    fix axios promise

--- pages/api/ContentKit.js
+++ pages/api/ContentKit.js
@@ -5,8 +5,12 @@ const API_KEY = process.env.CONVERTKIT_PUBLIC
 const URL = `https://api.convertkit.com/v3/forms/${FORM_ID}/subscribe`
 
 export default function ContentKit(req, res) {
-  const response = axios
+  return axios
     .post(URL, { api_key: API_KEY, email: req.body.email })
-    .then((response) => res.status(response.status).json('User Subscribed'))
-    .catch((err) => res.status(err.status).json('An error has occurred'))
+    .then((response) => {
+      res.status(200).json('User Subscribed')
+    })
+    .catch((err) => {
+      res.status(err.status).json('An error has occurred')
+    })
 }
